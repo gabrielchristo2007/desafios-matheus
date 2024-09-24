7de486b11f5e8811daaf1188fcd44c943b8ba2cf
gabrielchristo <christocoelhopereira2000@gmail.com>
2024-09-24 11:38:58 -0300

    .

---
+++ vogaisjs.js
@@ -0,0 +1,13 @@
+function vogal(txt){
+    const contagem = {}
+    let letra = document.getElementById('txt')
+    let botao = document.getElementById('txtbtn')
+    let res = document.getElementById('txtres')
+
+   
+
+    if(letra.value.length == 0) {
+       window.alert('Preencha todos os campos')
+    } 
+
+}
